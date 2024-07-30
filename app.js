e797ec600ec9331c03c5a341f4d1090c6e78f1f1
Aarthi20 <aarthi2000cse@gmail.com>
2024-07-30 14:47:49 +0530

    hello world

---
+++ app.js
@@ -0,0 +1,14 @@
+const express  = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    console.log(request);
+    response.send("Hello World!");
+})
+
+// Additionally added get today's date
+app.get("/date", (request, response) => {
+    let date = new Date();
+    response.send(`Today date is : ${date}`);
+})
+app.listen(3000);
